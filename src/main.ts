55b71000dab0bdbf6c4e922340d282d606ab3991
s-samadi <s-samadi@github.com>
2023-05-01 17:27:25 +1000

    work in progress:

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,12 @@ import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const frequency: number = Number(core.getInput('frequency'))
+    core.debug(`The frequency input is ${frequency}`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+  
 
     core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
+   // await wait(parseInt(frequency, 10))
     core.debug(new Date().toTimeString())
 
     core.setOutput('time', new Date().toTimeString())
